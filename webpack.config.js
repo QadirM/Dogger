4936ad6cfcb835e0c2aef165ad88d7bfd73987a1
Qadir Mohammed-Ali <a.mohammedali@outlook.com>
2017-03-28 10:37:37 -0700

    Setup webpack, package.json, and write a basic entry file

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: "./lib/dogger.js",
+  output: {
+  	filename: "./lib/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  }
+};
